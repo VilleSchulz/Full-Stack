1ab6e13f219f0cc360cf44bab0c17ad5db3b7b35
VilleSchulz <villemsc@metropolia.fi>
2024-07-01 18:13:04 +0300

    2.5

---
+++ part2/2.6-/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] useState(
+    'a new note...'
+  )
+const addNote = (event) =>{
+  event.preventDefault()
+  console.log("button clicked", event.target)
+}
+
+const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+}
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <ul>
+        <form onSubmit ={addNote}>
+          <input value = {newNote} 
+          onChange ={handleNoteChange}
+          />
+          <button type ="submit">save</button>
+        </form>
+      </ul>
+    </div>
+  )
+}
+
+export default App 
