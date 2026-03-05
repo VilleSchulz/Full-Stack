b29a5d3ba5afa82fa5061bd2ceb58518cb3a620b
VilleSchulz <Villesc@gmail.com>
2026-03-05 17:49:10 +0200

    2.12-2.17 done

--- part2/2.12-2.15/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ({ person, number, removeNumber }) => {
-  console.log("Person", person)
-  const personInfo = `${person}: ${number}`
-  return (
-    <div>{personInfo}<button onClick={removeNumber}>delete</button></div>
-  )
-}
-
-export default Persons
