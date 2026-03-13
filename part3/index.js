faff20d0be85388d7db9572dd88ebbc2efc02ca5
VilleSchulz <Villesc@gmail.com>
2026-03-13 14:14:52 +0200

    3.7-3.8 done

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,32 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
+
+
+
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+    return ''
+})
+
+const logger = morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.body(req, res)
+    ].join(' ')
+})
 app.use(express.json())
+app.use(logger)
+
+
+
 
 
 let persons = [
